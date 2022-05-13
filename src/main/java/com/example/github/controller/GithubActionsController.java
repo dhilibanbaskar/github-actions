214b82d430c1c4bba4d0219f5baae50031c6f1ff
dhilibanbaskar <dhiliban.b@payarc.in>
2022-05-13 19:04:51 +0530

    added test cases

---
+++ src/main/java/com/example/github/controller/GithubActionsController.java
@@ -0,0 +1,8 @@
+package com.example.github.controller;
+
+public class GithubActionsController {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/example/github/validationTest/ValidationTest.java
@@ -0,0 +1,68 @@
+package com.example.github.validationTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.github.model.Actions;
+
+@SpringBootTest
+public class ValidationTest {
+
+	@Test
+	void testValidateName() {
+
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		Actions actions = Actions.builder().name(null).number("9894").address("dark horse street").build();
+
+		Set<ConstraintViolation<Actions>> results = validator.validate(actions);
+
+		results.forEach(result -> {
+
+			System.out.println(result.getMessage());
+		});
+		assertEquals(1, results.size());
+	}
+
+	@Test
+	void testValidateNumber() {
+
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		Actions actions = Actions.builder().name("Drake Scott").number("98943312").address("Dark horse street").build();
+
+		Set<ConstraintViolation<Actions>> results = validator.validate(actions);
+		results.forEach(result -> {
+
+			System.out.println(result.getMessage());
+		});
+		assertEquals(2, results.size());
+	}
+
+	@Test
+	void testValidateAddress() {
+
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		Actions actions = Actions.builder().name("Drake Scott").number("98947752").address("Dark horse street 1983").build();
+
+		Set<ConstraintViolation<Actions>> results = validator.validate(actions);
+		results.forEach(result -> {
+
+			System.out.println(result.getMessage());
+		});
+		assertEquals(3, results.size());
+	}
+}
